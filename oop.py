ed8b6484271bd9d7883397d82259622e1ea78dda
Fikus <dima.aparin.98@mail.ru>
2024-09-26 19:55:48 +0300

    Attributes

--- oop.py
+++ oop.py
@@ -6,6 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_hw(self, lecturer, course, grade):
+        marks = [0,1,2,3,4,5,6,7,8,9,10]
+        if isinstance(lecturer, Lecturer) and grade in marks:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'        
         
 class Mentor:
     def __init__(self, name, surname):
@@ -13,6 +23,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+
+
+class Lecturer(Mentor):
+    def __init__(self,name,surname):
+        super().__init__(name,surname)
+        self.grades = {} 
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,14 +39,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-    def __init__(self,name,surname):
-        super().__init__(name,surname)
-
-class Reviewer(Mentor):
-    def __init__(self,name,surname):
-        super().__init__(name,surname)
 
         
 best_student = Student('Ruoy', 'Eman', 'your_gender')
